4a2737ba4a5bfdff286ae14623c1db104dfcfbff
Niranjan Kumar <niranjan.kushwaha@mediassist.in>
2022-02-17 20:47:24 +0530

    showed more text on condition

--- src/app/components/constants/common.constants.ts
+++ src/app/components/constants/common.constants.ts
@@ -6,16 +6,28 @@ export class CommonConstant {
       src: '../../../assets/images/Volunteers_Img/niranjan_img.jpg',
       name: 'Niranjan Kushwaha',
       designation: 'Software Engineer',
-      about: 'Hello My name is Niranjan Kushwaha',
+      about: `
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus esse consequatur dolorum voluptate cum tempore labore ab dignissimos vitae, culpa cumque beatae, maiores placeat alias libero. Asperiores maxime debitis iusto.
+      Enim amet earum laudantium adipisci eveniet sequi ad minus officiis ex dolor consequuntur ducimus tempora quisquam rem ab natus qui repellat, error, pariatur delectus voluptas odit exercitationem vero? Recusandae, eum?
+      Enim accusamus illum, ea accusantium quae voluptatem maxime quam doloribus qui illo possimus explicabo, in rem ipsam dolor sapiente minima exercitationem cumque iure architecto temporibus aut animi facilis? Omnis, rem!
+      Ullam, libero deleniti. Libero minus nostrum reprehenderit corrupti maiores accusantium expedita, similique aliquam. Nulla doloremque quae aspernatur assumenda in totam vel suscipit quaerat tempore. Error ea architecto vel commodi fugit.
+      Delectus qui perspiciatis doloribus, temporibus impedit voluptas odit explicabo natus reiciendis repellat! Reprehenderit ipsam tempore, ea aut corrupti adipisci at sequi, corporis necessitatibus porro provident in, ad nobis accusamus? Dolorem.
+      Provident vero neque dolorem in iure at earum perferendis eligendi aliquam sunt ut temporibus eaque esse, quos saepe. Dolores nam quod ipsam maxime illum nobis voluptate esse sed dolorum quia.
+      Iste quasi numquam fugit repellendus pariatur officiis sapiente enim optio consequuntur velit porro magnam id voluptates, ipsa tempore eligendi quia sit, molestias eos? Iusto necessitatibus velit asperiores quidem beatae a.
+      Similique praesentium dolorem voluptatibus nam molestiae sed minima, quasi ut, modi maiores odit placeat nulla quaerat, sint expedita nemo perspiciatis. Assumenda iure possimus facilis quia dicta deleniti dignissimos perspiciatis fuga?
+      Odio libero provident unde animi eius adipisci itaque in, magnam commodi. Corporis sequi, accusamus cupiditate eos tempore laudantium doloribus iusto consectetur beatae, architecto aut numquam quia placeat harum minima commodi.
+      Ipsam veritatis iusto tempore, dolor totam aspernatur necessitatibus eos labore sint porro. Quas maiores perspiciatis nihil! Necessitatibus, laudantium. Quam at nobis rem, quo necessitatibus quod eius consequatur! Cum, animi illo.`,
       hasLink: '',
     },
+
     {
       id: 2,
       src: '../../../assets/images/Volunteers_Img/rajimg.jpg',
       name: 'Rajesh Kushwaha',
       designation: 'Software Engineer',
       about: 'Hello My name is Rajesh Kushwaha',
-      hasLink: '',
+      hasLink:
+        'https://stackoverflow.com/questions/25957073/how-to-increase-div-width-according-to-the-text-inside-it',
     },
     {
       id: 3,
--- src/app/components/volunteers-card/volunteers-card.component.ts
+++ src/app/components/volunteers-card/volunteers-card.component.ts
@@ -9,5 +9,32 @@ export class VolunteersCardComponent implements OnInit {
   @Input() data: any;
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.data.map((el: any) => {
+      let totalWordsArr = el.about.split(' ');
+      el.totalWordsArr = totalWordsArr;
+      if (totalWordsArr.length > 10) {
+        el.showMore = true;
+        let count = 0;
+        let firstTenWords = '';
+        totalWordsArr.forEach((word: any) => {
+          if (word && count < 10) {
+            firstTenWords += word + ' ';
+            count += 1;
+            if (count === 10) {
+              firstTenWords += '...';
+            }
+          }
+        });
+        el.firstTenWords = firstTenWords;
+      } else {
+        el.firstTenWords = el.about;
+        el.showMore = false;
+      }
+    });
+  }
+
+  openLink(url: string) {
+    window.open(url);
+  }
 }
