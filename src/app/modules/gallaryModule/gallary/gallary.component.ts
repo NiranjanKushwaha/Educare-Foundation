9e288ca67cff811576812134ffcca967d916eac0
Niranjan Kumar <niranjan.kushwaha@mediassist.in>
2022-02-17 18:27:38 +0530

    code modified

--- src/app/modules/gallaryModule/gallary/gallary.component.ts
+++ src/app/modules/gallaryModule/gallary/gallary.component.ts
@@ -1,15 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-
+import { CommonService } from 'src/app/services/common.service';
 @Component({
   selector: 'app-gallary',
   templateUrl: './gallary.component.html',
-  styleUrls: ['./gallary.component.css']
+  styleUrls: ['./gallary.component.css'],
 })
 export class GallaryComponent implements OnInit {
-
-  constructor() { }
+  constructor(private commonService: CommonService) {}
 
   ngOnInit(): void {
+    this.commonService.hideVolunteer.next(true);
   }
-
 }
